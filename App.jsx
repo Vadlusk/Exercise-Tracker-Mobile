3a89b7fb5b64b192abcae20ed20593bae8ef42fa
AdamLusk-Trane <adamlusk.trane@gmail.com>
2022-07-09 19:44:22 -0600

    :heavy_plus_sign: chore: add eslint

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+import Main from './app/screens/Main'
+
+export default function App () {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Main />
+    </>
+  )
+}
